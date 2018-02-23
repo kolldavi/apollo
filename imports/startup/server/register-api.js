3904132afa9e2506948654d113a7115d501ca215
kolldavi <kolldavi@gmail.com>
2018-02-23 06:40:04 -0600

    add resolutions graphql schema

--- imports/startup/server/register-api.js
+++ imports/startup/server/register-api.js
@@ -1,1 +1,36 @@
-import ResolutionsSchema from '../../resolutions/ResolutionsSchema.graphql';
+import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql';
+import { createApolloServer } from 'meteor/apollo';
+import { makeExecutableSchema } from 'graphql-tools';
+const testData = `
+type Query{
+  hi: String
+  resolutions:[Resolution]
+}
+`;
+const typeDefs = [testData, ResolutionsSchema];
+
+const resolvers = {
+  Query: {
+    hi() {
+      return 'Hello App';
+    },
+    resolutions() {
+      return [
+        {
+          _id: 'anyString',
+          name: "Let's see if this works"
+        },
+        {
+          _id: 'anyString2',
+          name: "Let's see if this works2"
+        }
+      ];
+    }
+  }
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+createApolloServer({ schema });
